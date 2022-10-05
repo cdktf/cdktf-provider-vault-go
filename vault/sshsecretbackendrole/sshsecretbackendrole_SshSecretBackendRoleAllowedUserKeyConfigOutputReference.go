40271981b05bc29a8bd58509fc21435049c9a301
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-10-05 00:47:26 +0000

    chore(release): v3.0.2

--- vault/sshsecretbackendrole/sshsecretbackendrole_SshSecretBackendRoleAllowedUserKeyConfigOutputReference.go
+++ vault/sshsecretbackendrole/sshsecretbackendrole_SshSecretBackendRoleAllowedUserKeyConfigOutputReference.go
@@ -2,9 +2,9 @@ package sshsecretbackendrole
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-vault-go/vault/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-vault-go/vault/v3/jsii"
 
-	"github.com/hashicorp/cdktf-provider-vault-go/vault/v3/sshsecretbackendrole/internal"
+	"github.com/cdktf/cdktf-provider-vault-go/vault/v3/sshsecretbackendrole/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
